2dac3a719a93b853cb924272f7b961b5e9ad2d2d
Billings <k_billings@tss-hr420-dm11.fc.ca>
2017-11-29 16:48:27 -0500

    Ajax commit basic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,61 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-  var Img = document.querySelectorAll('.data-ref'),
-  model = document.querySelector('.modelName'),
-  price = document.querySelector('.priceInfo'),
-  details = document.querySelector('.modelDetails');
+  var Img = document.querySelectorAll('.data-ref');
+
+  const httpRequest = new XMLHttpRequest();
 
   function changeModel () {
+    //1. make an AJAX call to the databse; handle any errors first
+    if (!httpRequest) {
+      alert('giving up, your browser sucks!');
+      return false;
+    }
+
+    httpRequest.onreadystatechange = processRequest;
+    httpRequest.open('GET', './includes/functions.php?carModel=' +this.id);
+    httpRequest.send();
+  }
+
+  function processRequest() {
+    // handle the stages of our AJAX call
+    let reqIndicator = document.querySelector('.request-state');
+    reqIndicator.textContent = httpRequest.readyState;
+
+    //debugger;
+
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {// request worked, all is good
+        //debugger;
+        let data = JSON.parse(httpRequest.responseText);
+        processResult(data);
+      } else {
+        alert('There was a problem with the request');
+      }
+    }
+  }
+
+  function processResult(data) {
+    //debugger;
+    const { modelName, pricing, modelDetails} = data;
+    // statement chaining lets you go lots of stuff on one line
+    let model = document.querySelector('.modelName').textContent = modelName;
+    let price = document.querySelector('.priceInfo').innerHTML = pricing;
+    let details = document.querySelector('.modelDetails').textContent = modelDetails;
     //debugger;
     //changeModel text
     let objectIndex = carData[this.id];
 
-    model.firstChild.nodeValue = objectIndex.model;
-    price.firstChild.nodeValue = objectIndex.price;
-    details.firstChild.nodeValue = objectIndex.description;
+    //model.firstChild.nodeValue = objectIndex.model;
+    //price.firstChild.nodeValue = objectIndex.price;
+    //details.firstChild.nodeValue = objectIndex.description;
 
     //opacity change
-    
+
     Img.forEach(function(element, index) {
       element.classList.remove("focusMini");
     });
-    this.classList.add("focusMini");
+    // the backticks are a new ES6 thing called a template string (look it up)
+    document.querySelector(`#${data.model}`).classList.add("focusMini");
 
     //console.log(this.id);
   }
